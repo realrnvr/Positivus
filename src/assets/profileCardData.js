a315d0ac3bfe73ed362d5c3eb5e9d6b5cdb93b4b
realrnvr <realrnvr@gmail.com>
2024-09-06 16:15:30 +0530

    image format changes

--- src/assets/profileCardData.js
+++ src/assets/profileCardData.js
@@ -1,55 +1,56 @@
 export const profileCardData = [
-    {
-        id: 1,
-        name: "John Smith",
-        position: "CEO and Founder",
-        profileImg: "/pfp-1.svg",
-        profileImgAlt: "John Smith's profile picture",
-        description: "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
-    },
-    {
-        id: 2,
-        name: "Jane Doe",
-        position: "Director of Operations",
-        profileImg: "/pfp-2.svg",
-        profileImgAlt: "Jane Doe's profile picture",
-        description: "7+ years of experience in project management and team leadership. Strong organizational and communication skills",
-
-    },
-    {
-        id: 3,
-        name: "Michael Brown",
-        position: "Senior SEO Specialist",
-        profileImg: "/pfp-3.svg",
-        profileImgAlt: "Michael Brown's profile picture",
-        description: "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization",
-
-    },
-    {
-        id: 4,
-        name: "Emily Johnson",
-        position: "PPC Manager",
-        profileImg: "/pfp-4.svg",
-        profileImgAlt: "Emily Johnson's profile picture",
-        description: "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
-
-    },
-    {
-        id: 5,
-        name: "Brian Williams",
-        position: "Social Media Specialist",
-        profileImg: "/pfp-5.svg",
-        profileImgAlt: "Brian Williams's profile picture",
-        description: "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement",
-
-    },
-    {
-        id: 6,
-        name: "Sarah Kim",
-        position: "Content Creator",
-        profileImg: "/pfp-6.svg",
-        profileImgAlt: "Sarah Kim's profile picture",
-        description: "2+ years of experience in writing and editing, Skilled in creating compelling, SEO-optimized content for various industries",
-
-    }
-]
+  {
+    id: 1,
+    name: "John Smith",
+    position: "CEO and Founder",
+    profileImg: "/pfp-1.webp",
+    profileImgAlt: "John Smith's profile picture",
+    description:
+      "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    position: "Director of Operations",
+    profileImg: "/pfp-2.webp",
+    profileImgAlt: "Jane Doe's profile picture",
+    description:
+      "7+ years of experience in project management and team leadership. Strong organizational and communication skills",
+  },
+  {
+    id: 3,
+    name: "Michael Brown",
+    position: "Senior SEO Specialist",
+    profileImg: "/pfp-3.webp",
+    profileImgAlt: "Michael Brown's profile picture",
+    description:
+      "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization",
+  },
+  {
+    id: 4,
+    name: "Emily Johnson",
+    position: "PPC Manager",
+    profileImg: "/pfp-4.webp",
+    profileImgAlt: "Emily Johnson's profile picture",
+    description:
+      "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
+  },
+  {
+    id: 5,
+    name: "Brian Williams",
+    position: "Social Media Specialist",
+    profileImg: "/pfp-5.webp",
+    profileImgAlt: "Brian Williams's profile picture",
+    description:
+      "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement",
+  },
+  {
+    id: 6,
+    name: "Sarah Kim",
+    position: "Content Creator",
+    profileImg: "/pfp-6.webp",
+    profileImgAlt: "Sarah Kim's profile picture",
+    description:
+      "2+ years of experience in writing and editing, Skilled in creating compelling, SEO-optimized content for various industries",
+  },
+];
