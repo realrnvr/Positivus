d9ab7cc1281ee910ea238d842927a63ffa7d8248
realrnvr <realrnvr@gmail.com>
2024-09-07 10:29:36 +0530

    imports updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
-import Header from "./components/header/Header";
-import Home from "./pages/home/Home";
+import {
+  Home,
+  Services,
+  Case,
+  Work,
+  Team,
+  Testimonial,
+  Contact,
+} from "./pages/imports";
+
 import Carousel from "./components/carousel/Carousel";
-import Services from "./pages/services/Services";
-import Case from "./pages/case/Case";
-import Work from "./pages/work/Work";
-import Team from "./pages/team/Team";
-import Testimonial from "./pages/testimonial/Testimonial";
-import Contact from "./pages/contact/Contact";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
---
+++ src/pages/imports.js
@@ -0,0 +1,7 @@
+export { default as Case } from "./case/Case.jsx";
+export { default as Contact } from "./contact/Contact.jsx";
+export { default as Home } from "./home/Home.jsx";
+export { default as Services } from "./services/Services.jsx";
+export { default as Team } from "./team/Team.jsx";
+export { default as Testimonial } from "./testimonial/Testimonial.jsx";
+export { default as Work } from "./work/Work.jsx";
